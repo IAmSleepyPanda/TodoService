3c65eb8f35662aea864404256e5df340b2a18696
IAmSleepyPanda <59969537+IAmSleepyPanda@users.noreply.github.com>
2021-10-11 13:54:49 +0600

    Update BaseManager.cs

--- TodoProject.BLL.Common/BaseManager.cs
+++ TodoProject.BLL.Common/BaseManager.cs
@@ -3,17 +3,17 @@
 namespace TodoProject.BLL.Common
 {
     /// <summary>
-    /// 
+    /// Provides base manager
     /// </summary>
     public class BaseManager
     {
         /// <summary>
-        /// 
+        /// Returns success manager result
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="result"></param>
-        /// <param name="message"></param>
-        /// <returns></returns>
+        /// <typeparam name="T">Type of result model</typeparam>
+        /// <param name="result">Result model</param>
+        /// <param name="message">Message</param>
+        /// <returns>Success manager result</returns>
         public ManagerResult<T> Ok<T>(T result, string message = null) =>
             new ManagerResult<T>
             {
@@ -23,9 +23,9 @@ namespace TodoProject.BLL.Common
             };
 
         /// <summary>
-        /// 
+        /// Returns success manager result
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Success manager result</returns>
         public ManagerResult Ok() =>
             new ManagerResult
             {
@@ -34,11 +34,11 @@ namespace TodoProject.BLL.Common
 
 
         /// <summary>
-        /// 
+        /// Returns failed manager result
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="message"></param>
-        /// <returns></returns>
+        /// <typeparam name="T">Type of result model</typeparam>
+        /// <param name="message">Error message</param>
+        /// <returns>Error manager result</returns>
         public ManagerResult<T> OperationFailed<T>(string message) =>
             new ManagerResult<T>
             {
@@ -47,10 +47,10 @@ namespace TodoProject.BLL.Common
             };
 
         /// <summary>
-        /// 
+        /// Returns error manager result
         /// </summary>
-        /// <param name="message"></param>
-        /// <returns></returns>
+        /// <param name="message">Error message</param>
+        /// <returns>Error manager result</returns>
         public ManagerResult OperationFailed(string message) => new ManagerResult
         {
             Message = message,
